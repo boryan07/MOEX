a8228c128c012e82f5a177438da701ceea01bb15
Владимир Царьков <askerotwo@gmail.com>
2020-09-15 00:48:11 +0300

    1.01

--- untitled1/src/main/java/springboot/topjava/ru/Controller.java
+++ untitled1/src/main/java/springboot/topjava/ru/Controller.java
@@ -1,10 +1,13 @@
 package springboot.topjava.ru;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 
 @RestController
@@ -12,6 +15,8 @@ public class Controller {
 
     @Value("${link}")
     private String link;
+    @Value("${timeout}")
+    private int timeout;
 
     private String name = "default_name";
     private final String DEFAULT_RETURN_VALUE = "Default value";
@@ -22,15 +27,25 @@ public class Controller {
     }
 
     @RequestMapping("/greet")
-    public Response pay() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setErrorHandler(new ErrorHandler());
-        ResponseEntity<Void> page = restTemplate.getForEntity(link, Void.class);
+    public Response pay(){
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        Duration duration = Duration.ofMillis(timeout);
+        RestTemplate restTemplate = restTemplateBuilder.setConnectTimeout(duration)
+                .setReadTimeout(duration)
+                .errorHandler(new ErrorHandler())
+                .build();
+        ResponseEntity<Void> page;
+        try {
+            page = restTemplate.getForEntity(link, Void.class);
+        }
+        catch (Exception e) {
+            return new Response("Никого нет дома");
+        }
         String returnValue = DEFAULT_RETURN_VALUE;
         if (page.getStatusCode().equals(HttpStatus.OK)) {
             returnValue = name + ", сервис приветствует тебя";
         } else if (page.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
-            returnValue = "Извините, "+name + ", но я вас не знаю";
+            returnValue = "Извините, " + name + ", но я вас не знаю";
         }
         return new Response(returnValue);
     }
--- untitled1/src/main/java/springboot/topjava/ru/ErrorHandler.java
+++ untitled1/src/main/java/springboot/topjava/ru/ErrorHandler.java
@@ -2,7 +2,6 @@ package springboot.topjava.ru;
 
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.ResponseErrorHandler;
 
 import java.io.IOException;
 
